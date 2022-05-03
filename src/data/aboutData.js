7100c55bdfc91b2b22a1ff239b8a45eeb8e15f8b
S21Dr <adel.ernandies@mail.ru>
2022-05-03 19:50:21 +0300

    add app

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Кто я",
+    description1: "Меня зовут Алина. Я главный врач-диетолог Чувашии.",
+    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    image: 2
+}
---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'vityazcheb1@mail.ru',
+    phone: '+79196606315',
+    address: 'Menlo Park, California, United States - 673822 ',
+
+    sheetAPI: ''
+}
